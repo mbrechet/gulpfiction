acdb76ad3e4d0d26e4a71c939db35f1a5af4ffd3
mbrechet <m.brechet@gmail.com>
2015-05-05 11:45:19 +0200

    Add gulp file + bootstrap sass theme

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+var config = {
+	bootstrapDir: './node_modules/bootstrap-sass',
+	publicDir: './client'
+};
+
+
+// scss link
+gulp.task('scss',function(){
+	return gulp.src('./client/sass/app.scss')
+	.pipe(sass({
+		includePaths:[config.bootstrapDir+ '/assets/stylesheets']
+	})).pipe(
+		gulp.dest(config.publicDir+'/css')
+	);
+});
+
+// fonts link
+gulp.task('fonts', function() {
+    return gulp.src(config.bootstrapDir + '/assets/fonts/**/*')
+    .pipe(gulp.dest(config.publicDir + '/fonts'));
+});
+
+gulp.task('default',['scss', 'fonts']);
