027aa2264e92d7b3b4558a034547a8f71377107f
mbrechet <m.brechet@gmail.com>
2015-05-19 10:10:34 +0200

    Add js build
    moved js files en sass files in client-src
    upadte gulp to build css and js into client directory
    add to git ignore the files generated in client directory

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,25 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var uglify = require('gulp-uglify');
 
 var config = {
 	bootstrapDir: './node_modules/bootstrap-sass',
-	publicDir: './client'
+	publicDir: './client',
+	releaseDir: './release/'
 };
 
 
 // scss link
 gulp.task('scss',function(){
-	return gulp.src('./client/sass/styles.scss')
+	return gulp.src('./client-src/sass/styles.scss')
 	.pipe(sass({
 		includePaths:[config.bootstrapDir+ '/assets/stylesheets'],
 		indentSynthax:true,
 		errLogToConsole:true
 	})).pipe(
 		gulp.dest(config.publicDir+'/css')
+	).pipe(
+		gulp.dest(config.releaseDir+'/css')
 	);
 });
 
@@ -25,4 +29,10 @@ gulp.task('fonts', function() {
     .pipe(gulp.dest(config.publicDir + '/fonts'));
 });
 
-gulp.task('default',['scss', 'fonts']);
+gulp.task('js-build',function(){
+	return gulp.src('./client-src/js/**/*.js')
+	.pipe(uglify())
+	.pipe(gulp.dest(config.publicDir+'/js'));
+});
+
+gulp.task('default',['scss', 'fonts', "js-build"]);
