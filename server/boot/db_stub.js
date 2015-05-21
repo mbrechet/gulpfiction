5d964df9ac032f41361ba843bfd8ae7237c5bdfd
mbrechet <m.brechet@gmail.com>
2015-05-21 13:32:28 +0200

    update stub

--- server/boot/db_stub.js
+++ server/boot/db_stub.js
@@ -4,11 +4,11 @@ module.exports = function(app) {
         'id': 1
     }]);
 
-     app.models.Movie.create({
+     app.models.Movie.create([{
         'title' : 'Pulp Fiction',
         'id': 1,
         'description' : 'Pulp Fiction, ou Fiction pulpeuse au Québec, est un film de gangsters américain réalisé par Quentin Tarantino et sorti en 1994. Le scénario est co-écrit par Tarantino et Roger Avary',
         'productionDate': '1994',
         'cover': 'object'
-    });
+    }]);
 };
