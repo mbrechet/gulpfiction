582d36c0596d73b8dadb7be8292abcefa830f200
mbrechet <m.brechet@gmail.com>
2015-05-28 17:04:38 +0200

    move html and images to src dir

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,30 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
+var clean = require('gulp-clean');
+var runSequence = require('run-sequence');
 
 var config = {
 	bootstrapDir: './node_modules/bootstrap-sass',
 	publicDir: './client',
+	sourceDir: './src',
 	releaseDir: './release/'
 };
 
+gulp.task('clean', function(){
+	return gulp.src(config.publicDir+'/**/*')
+        .pipe(clean({force: true}));
+});
+
+gulp.task('image', function(){
+	return gulp.src(config.sourceDir + '/img/**/*')
+    .pipe(gulp.dest(config.publicDir + '/img'));
+})
+
+gulp.task('html', function(){
+	return gulp.src(config.sourceDir+'/html/**/*')
+	.pipe(gulp.dest(config.publicDir+'/'))
+})
 
 // scss link
 gulp.task('scss',function(){
@@ -31,23 +48,33 @@ gulp.task('fonts', function() {
 });
 
 gulp.task('external-lib',function(){
-	return gulp.src('./src/js/libs/*')
+	return gulp.src(config.sourceDir+'/js/libs/*')
 	.pipe(uglify())
 	.pipe(gulp.dest(config.publicDir+'/js/libs'));
 });
 
 
 gulp.task('external-plugins',function(){
-	return gulp.src('./src/js/plugins/*')
+	return gulp.src(config.sourceDir+'/js/plugins/*')
 	.pipe(uglify())
 	.pipe(gulp.dest(config.publicDir+'/js/plugins'));
 });
 
 gulp.task('js-build',function(){
-	return gulp.src('./src/js/*')
+	return gulp.src(config.sourceDir+'/js/*')
 	.pipe(concat('scripts.js'))
 	.pipe(uglify({mangle:false}))
 	.pipe(gulp.dest(config.publicDir+'/js'));
 });
 
-gulp.task('default',['scss', 'fonts', 'js-build', 'external-lib', 'external-plugins']);
+gulp.task('default',function(callback){
+	runSequence(
+	'clean',
+	'image',
+	'html', 
+	'scss', 
+	'fonts', 
+	'external-lib', 
+	'external-plugins', 
+	'js-build', callback);
+});
