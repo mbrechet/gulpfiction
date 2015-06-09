ec6e562530f11d78e7ba86af6d6183b2a5d461d4
mbrechet <m.brechet@gmail.com>
2015-06-09 14:05:00 +0200

    create builder directory

---
+++ builder/simpleGulp.js
@@ -0,0 +1,32 @@
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var del = require('del');
+
+var config = {
+    sourceDir: '../src',
+    publicDir: '../client'
+};
+
+
+gulp.task('clean', function () {
+    return function (done) {
+        del(config.publicDir, {
+            force: true,
+            dot: true
+        },
+        done);
+    };
+});
+
+gulp.task('js-build','clean',function(){
+    return gulp.src(config.sourceDir+'/js/*')
+    .pipe(concat('scripts.js'))
+    .pipe(uglify({mangle:false}))
+    .pipe(gulp.dest(config.publicDir+'/js'));
+});
+
+
+
+gulp.task('default','js-build');
+
