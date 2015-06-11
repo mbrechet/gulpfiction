ce20d74f55904b119c4d72383d954b32dec0ae2e
n-a-n <loic.truchot35@gmail.com>
2015-06-11 16:52:52 +0200

    gulpfile enhancement for demo

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,22 @@
+//Generic dependencies
+var del = require('del');
 var fs = require('fs');
 var path = require('path');
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var del = require('del');
 
+//Gulp dependencies
+var gulp = require('gulp');
 var concat = require('gulp-concat');
+var sass = require('gulp-sass');
 var gutil = require('gulp-util');
 var runSequence = require('run-sequence');
 
+//globals
 global.dir = {
 	bootstrap: path.join(__dirname, 'node_modules', 'bootstrap-sass'),
 	client: path.join(__dirname, 'client'),
 	sources: path.join(__dirname, 'src'),
 	release: path.join(__dirname, 'release')
 };
-
-
-var tasks = [
-/*	'clean',	*/
-	'html', 
-	'scss', 
-	'fonts', 
-	'external-lib', 
-	'external-plugins', 
-	'image',
-	'js-build',
-	'release-assembly'
-];
 
 //create a task directly in gulpfile
 gulp.task('clean', function () {
@@ -40,28 +30,28 @@ gulp.task('clean', function () {
 });
 
 //OR check all individual task-file in tasks folder, and create a gulp-task with it
+var tasks = [/*	'clean',*/'html', 'scss', 'fonts', 'external-lib', 'external-plugins', 'image','js-build','release-assembly'];
 tasks.forEach(function (task) {
 	gulp.task(task, require('./tasks/' + task));
 });
 
-
+//Default task : run a sequence of task
 gulp.task('default',function(callback){
 	runSequence(
 		'clean',
-		'image',
-		'html', 
+		['image','html'], 
 		'scss', 
 		'fonts', 
-		'external-lib', 
-		'external-plugins', 
+		['external-lib', 'external-plugins'],		 
 		'js-build', 
 		'release-assembly',
 		callback
 	);
 });
 
-gulp.task('faketask', require('./tasks/faketask'));
 
+//other tasks for demo
+gulp.task('faketask', require('./tasks/faketask'));
 gulp.task('watch-webfiles', require('./tasks/watch-webfiles'));
 
 
